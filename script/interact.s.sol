aa6326750e5e62225a42e199819516dee67583bd
CC <casinocompiler@gmail.com>
2024-09-24 22:03:06 +0100

    - init interactions script

---
+++ script/interact.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Script} from "@forge/src/Script.sol";
+import {DevOpsTools} from "@devops/src/DevOpsTools.sol";
+
+contract MerkleAirdropInteractions is Script{
+    
+}
